b57a7b400c14834e7c82d188dd93cfdd053a10b8
Fernando Pereira <ferpereira1@gmail.com>
2022-09-25 22:44:33 -0300

    criação do app e login com firebase

---
+++ lib/utils/validator.dart
@@ -0,0 +1,44 @@
+class Validator {
+  static String? validateName({required String? name}) {
+    if (name == null) {
+      return null;
+    }
+
+    if (name.isEmpty) {
+      return 'O nome não pode estar vazio';
+    }
+
+    return null;
+  }
+
+  static String? validateEmail({required String? email}) {
+    if (email == null) {
+      return null;
+    }
+
+    RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+
+    if (email.isEmpty) {
+      return 'O e-mail não pode estar vazio';
+    } else if (!emailRegExp.hasMatch(email)) {
+      return 'Digite um e-mail correto';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword({required String? password}) {
+    if (password == null) {
+      return null;
+    }
+
+    if (password.isEmpty) {
+      return 'A senha não pode estar vazia';
+    } else if (password.length < 6) {
+      return 'Digite uma senha com comprimento de pelo menos 6';
+    }
+
+    return null;
+  }
+}
